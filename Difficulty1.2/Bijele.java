a1eace4f6db056d84f22bbe0835735d551172642
G40 <iceboy323@gmail.com>
2018-09-23 15:07:11 -0400

    improved Bijele solution

--- Difficulty1.2/Bijele.java
+++ Difficulty1.2/Bijele.java
@@ -5,13 +5,7 @@ public class Bijele{
         int[] perfect = {1,1,2,2,2,8};
         
         for(int i = 0; i < pieces.length; i++){
-            pieces[i] = io.getInt();
-            if (pieces[i] == 0 ){
-                pieces[i] = perfect[i];
-            }
-            else{
-                pieces[i] = perfect[i] - pieces[i];
-            }
+            pieces[i] = perfect[i] - io.getInt();
             System.out.print(pieces[i]+ " ");
         }
         io.close();
